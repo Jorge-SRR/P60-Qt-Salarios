9bd5345d81523ea9b67d749dbf0972c6ebb6fadf
Jorge Reinoso <jreinosor1@est.ups.edu.ec>
2022-06-27 20:56:27 -0500

    Trabajo Finalizado

---
+++ recursos.cpp
@@ -0,0 +1,46 @@
+#include "recursos.h"
+
+Recursos::Recursos(QObject *parent)
+    : QAbstractItemModel(parent)
+{
+}
+
+QVariant Recursos::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    // FIXME: Implement me!
+}
+
+QModelIndex Recursos::index(int row, int column, const QModelIndex &parent) const
+{
+    // FIXME: Implement me!
+}
+
+QModelIndex Recursos::parent(const QModelIndex &index) const
+{
+    // FIXME: Implement me!
+}
+
+int Recursos::rowCount(const QModelIndex &parent) const
+{
+    if (!parent.isValid())
+        return 0;
+
+    // FIXME: Implement me!
+}
+
+int Recursos::columnCount(const QModelIndex &parent) const
+{
+    if (!parent.isValid())
+        return 0;
+
+    // FIXME: Implement me!
+}
+
+QVariant Recursos::data(const QModelIndex &index, int role) const
+{
+    if (!index.isValid())
+        return QVariant();
+
+    // FIXME: Implement me!
+    return QVariant();
+}
